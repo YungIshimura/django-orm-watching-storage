b29064f1ccd78ebf2ee360ebf516f68bd90dcc97
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-04-12 05:18:14 +0300

    Placed user data in storage

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,36 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils.timezone import localtime
+
+
+def get_duration(visit):
+    duration = (localtime() - visit.entered_at).total_seconds()
+    
+    return duration
+
+
+def format_duration(duration):
+    hours = int(duration//3600)
+    minutes = int((duration%3600)//60)
+    
+    return f"{hours}:{minutes}"
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    visits = Visit.objects.filter(leaved_at = None)
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    non_closed_visits = []
+    
+    for visit in visits:
+    
+        non_closed_visits.append(
+            {
+                'who_entered': visit.passcard,
+                'entered_at': visit.entered_at,
+                'duration': format_duration(get_duration(visit)),
+            }
+        )
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
